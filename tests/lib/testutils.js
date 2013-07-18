611c0bf3c4edfba0b1871f24e26f96e9bc0f9092
Christopher Williams <cwilliams@appcelerator.com>
2013-07-18 14:11:49 -0400

    Can't seem to get tests working on master either

--- tests/lib/testutils.js
+++ tests/lib/testutils.js
@@ -169,7 +169,7 @@ exports.initCodeProcessor = function (logger) {
 	CodeProcessor.init(undefined, {
 		exactMode: true
 	}, [{
-		path: path.resolve(path.join('..', '..', 'plugins', 'common-globals')),
+		path: path.resolve(path.join(__dirname, '..', '..', 'plugins', 'common-globals')),
 		options: {}
 	}], testLibAST);
 	Runtime._unknown = false;
